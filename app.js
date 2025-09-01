3514643c4670d8631f6d930de91c5615084be8df
saheer1234797 <saheerk769@gmail.com>
2025-09-01 11:32:57 +0530

    originupdate

--- app.js
+++ app.js
@@ -27,13 +27,14 @@ const __dirname = dirname(__filename);
 
 
 const FRONTEND = [
-    'http://localhost:3001', 
-    // 'https://attendanceqr-frontend.onrender.com' 
+    // 'http://localhost:3001', 
+     'https://attendanceqr-frontend.onrender.com' 
 ];
 
 
 app.use(cors({
-    origin: FRONTEND,
+    // origin: FRONTEND,
+    origin:"https://attendanceqr-frontend.onrender.com",
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE']
 }));
